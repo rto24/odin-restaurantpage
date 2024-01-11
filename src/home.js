39e1e3fbe348abba142c62843fbabb5a39b26f02
Richard <richardto180@gmail.com>
2024-01-11 14:04:22 -0500

    Fixed rendering issue

--- src/home.js
+++ src/home.js
@@ -1,8 +1,14 @@
 import { createHtml, mainContent } from "./index";
 
 function renderItems() {
-  const title = createHtml('h1', null, 'title', 'Restaurant Name');
-  const subTitle = createHtml('h3', null, 'sub-title', 'Welcome');
+  const titleCont = document.createElement('div');
+  const title = createHtml('h1', null, ['title'], 'Restaurant Name');
+  const subTitle = createHtml('h3', null, ['sub-title'], 'Welcome');
+
+  mainContent.appendChild(titleCont);
+
+  titleCont.appendChild(title);
+  titleCont.appendChild(subTitle);
 }
 
 export { renderItems as renderHome };
