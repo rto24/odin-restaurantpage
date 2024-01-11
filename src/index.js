613f15ddcb36b6782c15aea804e3a16f65de58a3
Richard <richardto180@gmail.com>
2024-01-11 14:05:02 -0500

    Fixed classList condition

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function createHtml(type, id, classList, content) {
   const element = document.createElement(type);
   if (id) element.id = id;
   if (classList)
-    arrayClasses.forEach((thisClass) => element.classList.add(thisClass));
+    classList.forEach((thisClass) => element.classList.add(thisClass));
   if (content) element.textContent = content;
 
   return element;
