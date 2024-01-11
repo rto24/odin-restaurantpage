b5e99f3cecc537faaf1d2f3e57a498f8a57e6047
Richard <richardto180@gmail.com>
2024-01-11 15:48:55 -0500

    Added menu button

--- src/home.js
+++ src/home.js
@@ -3,14 +3,19 @@ import { createHtml, mainContent } from "./index";
 function renderItems() {
   const titleCont = document.createElement('div');
   titleCont.classList.add('title-cont');
-  
+
+  // Titles
   const title = createHtml('h1', null, ['title'], 'Restaurant Name');
   const subTitle = createHtml('h3', null, ['sub-title'], 'Welcome');
+
+  // Menu button
+  const menuBtn = createHtml('button', null, ['menu-btn'], 'Menu');
 
   mainContent.appendChild(titleCont);
 
   titleCont.appendChild(title);
   titleCont.appendChild(subTitle);
+  titleCont.appendChild(menuBtn);
 }
 
 export { renderItems as renderHome };
