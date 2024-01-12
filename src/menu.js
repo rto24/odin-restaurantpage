b95b97a9e2c64b994eddd373aa8d36e28d6a01d0
Richard <richardto180@gmail.com>
2024-01-12 12:31:35 -0500

    Added menu contents to the main container

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,18 @@
 import { createHtml, mainContent } from "./index";
 
 function renderItems() {
-  const menuCont = document.createElement('div');
   const menuItems = ['pasta', 'pizza', 'pesto'];
   const menuList = document.createElement('ul');
 
   menuItems.forEach((item) =>
     menuList.appendChild(createHtml('li', null, ['menu-items'], item))
   );
+
+  const menuCont = document.createElement('div');
+  menuCont.classList.add('menu');
+  mainContent.appendChild(menuCont);
+  menuCont.appendChild(menuList);
+
 }
 
 export { renderItems as renderMenu };
